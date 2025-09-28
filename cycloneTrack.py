5ed08013a72787e866e59213811bfef00b564511
yutsz1203 <mervinyu3124@gmail.com>
2025-09-28 22:46:55 +0800

    updates

--- cycloneTrack.py
+++ cycloneTrack.py
@@ -15,8 +15,9 @@ def get_data(url):
     except requests.RequestException:
         return None
 
+
 def process_cyclone_dataframe(df, adjust_Time=True):
-    processed_df = df.copy()    
+    processed_df = df.copy()
 
     processed_df["MaximumWind"] = processed_df["MaximumWind"].str[:-4].astype(int)
     if adjust_Time:
@@ -28,8 +29,12 @@ def process_cyclone_dataframe(df, adjust_Time=True):
             .str.zfill(2)
             + ":00"
         )
-    processed_df["Latitude"] = processed_df["Latitude"].apply(lambda s: round(float(s[:-1]), 1))
-    processed_df["Longitude"] = processed_df["Longitude"].apply(lambda s: round(float(s[:-1]), 1))
+    processed_df["Latitude"] = processed_df["Latitude"].apply(
+        lambda s: round(float(s[:-1]), 1)
+    )
+    processed_df["Longitude"] = processed_df["Longitude"].apply(
+        lambda s: round(float(s[:-1]), 1)
+    )
 
     processed_df["DistanceToHK"] = processed_df.apply(
         lambda row: int(
@@ -47,17 +52,18 @@ def get_cyclones():
     data = xmltodict.parse(source.content)
 
     cyclones = data["TropicalCycloneList"]["TropicalCyclone"]
-
     cyclone_list = []
+    if isinstance(cyclones, list):
+        for cyclone in cyclones:
+            cyclone_list.append(cyclone["TropicalCycloneURL"])
 
-    for cyclone in cyclones:
-        cyclone_list.append(cyclone["TropicalCycloneURL"])
-    
+    elif isinstance(cyclones, dict):
+        return [cyclones["TropicalCycloneURL"]]
+
     return cyclone_list
 
 
 def get_cyclone_movement(data, issued_signal):
-
     cyclone_name = data["TropicalCycloneName"]
     curr_information = data["AnalysisInformation"]
 
@@ -69,7 +75,9 @@ def get_cyclone_movement(data, issued_signal):
         track_df = pd.DataFrame(past_information)
         track_df = process_cyclone_dataframe(track_df)
     else:
-        track_df = pd.read_excel(f"data/typhoon/{cyclone_name}/{cyclone_name}_track.xlsx")
+        track_df = pd.read_excel(
+            f"data/typhoon/{cyclone_name}/{cyclone_name}_track.xlsx"
+        )
         maxwind = int(curr_information["MaximumWind"][:-4])
         curr_hour = str((int(curr_information["Time"][11:13]) + 8) % 24).zfill(2)
         time = f'{curr_information["Time"][:10]}HKT{curr_hour}:00'
@@ -86,12 +94,14 @@ def get_cyclone_movement(data, issued_signal):
         ]
 
     print(track_df.tail())
-    track_df.to_excel(f"data/typhoon/{cyclone_name}/{cyclone_name}_track.xlsx", index=False)
+    track_df.to_excel(
+        f"data/typhoon/{cyclone_name}/{cyclone_name}_track.xlsx", index=False
+    )
 
     print(f"Latest updates for {cyclone_name}:\n{track_df.iloc[-1]}")
 
-def get_cyclone_forecast(data):
 
+def get_cyclone_forecast(data):
     cyclone_name = data["TropicalCycloneName"]
     curr_information = data["AnalysisInformation"]
     curr_intensity = curr_information["Intensity"]
@@ -104,16 +114,22 @@ def get_cyclone_forecast(data):
         else:
             print(forecast)
     forecast_df = pd.DataFrame(forecast_information)
-    forecast_df["MaximumWind"] = forecast_df["MaximumWind"].ffill().fillna(curr_maximum_wind)
+    forecast_df["MaximumWind"] = (
+        forecast_df["MaximumWind"].ffill().fillna(curr_maximum_wind)
+    )
     forecast_df["Intensity"] = forecast_df["Intensity"].ffill().fillna(curr_intensity)
-
 
     forecast_df = process_cyclone_dataframe(forecast_df, False)
     time = curr_information["Time"]
-    start = pd.to_datetime(time, utc=True).tz_convert("Asia/Hong_Kong") + pd.Timedelta(hours=1)
-    timestamps = pd.date_range(start=start, periods=len(forecast_df), freq='h')
-    forecast_df["Time"] = timestamps.strftime('%Y-%m-%dHKT%H:%M')
-    forecast_df.to_excel(f"data/typhoon/{cyclone_name}/{cyclone_name}_forecast.xlsx", index=False)
+    start = pd.to_datetime(time, utc=True).tz_convert("Asia/Hong_Kong") + pd.Timedelta(
+        hours=1
+    )
+    timestamps = pd.date_range(start=start, periods=len(forecast_df), freq="h")
+    forecast_df["Time"] = timestamps.strftime("%Y-%m-%dHKT%H:%M")
+    forecast_df.to_excel(
+        f"data/typhoon/{cyclone_name}/{cyclone_name}_forecast.xlsx", index=False
+    )
+
 
 def get_cyclone_info(url, issued_signal):
     while True:
@@ -135,7 +151,7 @@ def get_cyclone_info(url, issued_signal):
                 lat_dir = "more south"
             else:
                 lat_dir = "(as forecasted)"
-            
+
             if long_diff > 0:
                 long_dir = "more east"
             elif long_diff < 0:
@@ -143,15 +159,17 @@ def get_cyclone_info(url, issued_signal):
             else:
                 long_dir = "(as forecasted)"
 
-            print(f"Latitude: {abs(lat_diff)} {lat_dir}; Longitude: {abs(long_diff)}  {long_dir}")
-            
+            print(
+                f"Latitude: {abs(lat_diff)} {lat_dir}; Longitude: {abs(long_diff)}  {long_dir}"
+            )
+
             prev_forecast = data["ForecastInformation"][0]
             prev_lat = float(prev_forecast["Latitude"][:-1])
             prev_long = float(prev_forecast["Longitude"][:-1])
-            
+
             get_cyclone_movement(data, issued_signal)
             get_cyclone_forecast(data)
-            
+
             if issued_signal:
                 countdown(60, "cycloneTracking")
             else:
@@ -163,7 +181,7 @@ def get_cyclone_info(url, issued_signal):
 
 if __name__ == "__main__":
     cyclones = get_cyclones()
-    
+
     if len(cyclones) > 1:
         thread_list = []
         issued_signal = []
@@ -171,12 +189,14 @@ if __name__ == "__main__":
             data = get_data(cyclone)
             if data:
                 cyclone_name = data["TropicalCycloneName"]
-                issued = input(f"Is Tropical Cyclone Warning Signal No. 1 or above in force for {cyclone_name}? (y/n): ").lower()
+                issued = input(
+                    f"Is Tropical Cyclone Warning Signal No. 1 or above in force for {cyclone_name}? (y/n): "
+                ).lower()
                 while True:
-                    if issued == 'y':
+                    if issued == "y":
                         issued_signal.append(True)
                         break
-                    elif issued == 'n':
+                    elif issued == "n":
                         issued_signal.append(False)
                         break
                     else:
@@ -184,34 +204,31 @@ if __name__ == "__main__":
 
         for idx, cyclone in enumerate(cyclones):
             print(cyclone)
-            t = threading.Thread(target=get_cyclone_info, args=(cyclone, issued_signal[idx]))
+            t = threading.Thread(
+                target=get_cyclone_info, args=(cyclone, issued_signal[idx])
+            )
             thread_list.append(t)
             t.start()
-        
+
         for thread in thread_list:
             thread.join()
-        
-        else:
-            cyclone = cyclones[0]
-            data = get_data(cyclone)
-            if data:
-                cyclone_name = data["TropicalCycloneName"]
-                issued = input(f"Is Tropical Cyclone Warning Signal No. 1 or above in force for {cyclone_name}? (y/n): ").lower()
-                while True:
-                    if issued == 'y':
-                        issued_signal = True
-                        break
-                    elif issued == 'n':
-                        issued_signal = False
-                        break
-                    else:
-                        print("Invalid input. Please enter y or n.")
-            
-                get_cyclone_info(cyclone, issued_signal)
-        
 
-    
+    else:
+        cyclone = cyclones[0]
+        data = get_data(cyclone)
+        if data:
+            cyclone_name = data["TropicalCycloneName"]
+            issued = input(
+                f"Is Tropical Cyclone Warning Signal No. 1 or above in force for {cyclone_name}? (y/n): "
+            ).lower()
+            while True:
+                if issued == "y":
+                    issued_signal = True
+                    break
+                elif issued == "n":
+                    issued_signal = False
+                    break
+                else:
+                    print("Invalid input. Please enter y or n.")
 
-    
-    
-        
+            get_cyclone_info(cyclone, issued_signal)
